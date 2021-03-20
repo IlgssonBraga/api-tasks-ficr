8398331b8d321f4145b40094f1de53490adc7b3b
Ilgsson Braga <ilgssonbraga@localhost.localdomain>
2021-03-19 22:01:51 -0300

    feat: sequelize config

--- src/app/controllers/TodosController.js
+++ src/app/controllers/TodosController.js
@@ -1,7 +1,15 @@
+const { sequelize } = require("../../config/sequelize");
+
 class TodosController {
-    async index(req, res){
-        res.send('Todos')
+  async index(req, res) {
+    try {
+      await sequelize.authenticate();
+      console.log("Connection has been established successfully.");
+    } catch (error) {
+      console.error("Unable to connect to the database:", error);
     }
+    res.send("Todos");
+  }
 }
 
-module.exports = {TodosController}
+module.exports = { TodosController };
